bf9389f98d5475bb2d5084195b6a54cd91f9e53a
Tirage Royale <tirageroyale55@gmail.com>
2026-02-18 01:00:08 +0100

    final spa fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vite"; 
 import react from "@vitejs/plugin-react";
-
-
-
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'
-})
+  base: "./", // <-- important pour que le build pointe vers les bons fichiers
+  build: {
+    outDir: "dist", // normalement déjà par défaut
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000',
+    },
+  },
+});
