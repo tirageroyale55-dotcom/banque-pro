6521e6383593e44476475964fceee39178e2b616
Tirage Royale <tirageroyale55@gmail.com>
2026-02-24 22:23:55 +0100

    final spa fix

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#003A8F",
-        light: "#E6F0FF"
-      }
-    }
+    extend: {},
   },
-  plugins: []
-};
+  plugins: [],
+}
