c66ca7a6ed22aad97e24a1e93f8409cb5ab751e3
Tirage Royale <tirageroyale55@gmail.com>
2026-02-28 16:22:52 +0100

    fix admin token + images

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -2,11 +2,26 @@ const router = require("express").Router();
 const auth = require("../middleware/auth.middleware");
 const role = require("../middleware/role.middleware");
 const { rejectUser } = require("../controllers/admin.controller");
+const User = require("../models/User");
 
 const {
   validateUser,
   getPendingUsers
 } = require("../controllers/admin.controller");
+
+router.get("/user/:id", auth, role("ADMIN"), async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "Utilisateur introuvable" });
+    }
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: "Erreur serveur" });
+  }
+});
 
 router.get("/pending", auth, role("ADMIN"), getPendingUsers);
 router.post("/validate/:id", auth, role("ADMIN"), validateUser);
