7c5fb9ab71f7b0a7d37c146b4bfe54597b34d716
Tirage Royale <tirageroyale55@gmail.com>
2026-03-03 23:19:50 +0100

    fix import ForgotId case

--- frontend/src/pages/admin/AdminReset.jsx
+++ frontend/src/pages/admin/AdminReset.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { api } from "../services/api";
+import { api } from "../../services/api";
 
 export default function AdminReset() {
   const [email, setEmail] = useState("");
