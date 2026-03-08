491cbf08f3dae9ab56225b1380e2e2dfe36ce5c2
Tirage Royale <tirageroyale55@gmail.com>
2026-03-08 18:32:34 +0100

    fix import ForgotId case

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -1,18 +1,32 @@
 import { useLocation, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Welcome() {
 
   const navigate = useNavigate();
   const location = useLocation();
 
-  const user = location.state?.user;
+  const [user, setUser] = useState(location.state?.user || null);
 
   useEffect(() => {
-    if (!user) {
+
+    // si user venant du login
+    if (location.state?.user) {
+      localStorage.setItem("user", JSON.stringify(location.state.user));
+      setUser(location.state.user);
+      return;
+    }
+
+    // sinon on regarde dans le localStorage
+    const storedUser = localStorage.getItem("user");
+
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    } else {
       navigate("/login");
     }
-  }, [user, navigate]);
+
+  }, [location.state, navigate]);
 
   if (!user) return null;
 
@@ -22,7 +36,7 @@ export default function Welcome() {
       <div className="apply-card welcome-card">
 
         <h2 className="welcome-title">
-          Bienvenue {user.prenom} 
+          Bienvenue {user.prenom}
         </h2>
 
         <button
