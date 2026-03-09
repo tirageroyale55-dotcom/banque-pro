bc59c155c50cd82fee149cdb1cb938124bc57480
Tirage Royale <tirageroyale55@gmail.com>
2026-03-09 22:00:00 +0100

    fix import ForgotId case

--- frontend/src/components/BalanceBar.jsx
+++ frontend/src/components/BalanceBar.jsx
@@ -5,7 +5,7 @@ return (
 <div className={`balance-bar ${visible ? "show" : ""}`}>
   <div className="balance-content">
     <span>Solde disponible</span>
-    <strong>1000000 €</strong>
+    <strong>12345768987671000000 €</strong>
   </div>
 </div>
 
