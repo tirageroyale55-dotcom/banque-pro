2d38230c07733232c6f537fd32638668147ba2fc
Tirage Royale <tirageroyale55@gmail.com>
2026-03-13 18:22:51 +0100

    fix import ForgotId case

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-app.use("/api", cardRoutes);
+
 
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/admin", require("./routes/admin.routes"));
