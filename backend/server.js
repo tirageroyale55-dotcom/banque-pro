6b36e0cdc9f63a930ca47f2f85b92c358d92432d
Tirage Royale <tirageroyale55@gmail.com>
2026-03-13 18:20:41 +0100

    fix import ForgotId case

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const connectDB = require("./config/db");
+const cardRoutes = require("./routes/card.routes");
+
 
 console.log("MONGO_URI au démarrage =", JSON.stringify(process.env.MONGO_URI));
 
@@ -14,11 +16,14 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+app.use("/api", cardRoutes);
 
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/admin", require("./routes/admin.routes"));
 app.use("/api/client", require("./routes/client.routes"));
 app.use("/api/client", require("./routes/card.routes"));
+
+
 app.use("/api/transactions", require("./routes/transaction.routes"));
 
 const PORT = process.env.PORT || 5000;
