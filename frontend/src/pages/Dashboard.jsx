e5810a45168edbfdd1bfbb42b825ce5d1427d33c
Tirage Royale <tirageroyale55@gmail.com>
2026-03-23 10:05:48 +0100

    correction css card

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -122,8 +122,8 @@ setActiveTab={setActiveTab}
 />
 
 <BalanceBar
-balance={data.balance}
-visible={showBalanceBar}
+  balance={data.balance}
+  visible={true}
 />
 
 <div className="page-content" ref={contentRef}>
