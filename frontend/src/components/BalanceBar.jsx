8c2b8dca19789f1042439c4248777f0ed5ccf7f2
Tirage Royale <tirageroyale55@gmail.com>
2026-03-25 05:35:52 +0100

    correction css card

--- frontend/src/components/BalanceBar.jsx
+++ frontend/src/components/BalanceBar.jsx
@@ -2,7 +2,7 @@ export default function BalanceBar({ balance, visible }) {
 
 return (
 
-<div className={`balance-bar ${visible ? "show" : ""}`} style={{ border: '2px solid red' }}>
+<div className={`balance-bar ${visible ? "show" : ""}`}>
   <div className="balance-content">
     <span>Solde disponible</span>
     <strong>{balance} €</strong>
