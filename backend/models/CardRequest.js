3e2793daaacced07f6ce92e40e128422d23fd44f
Tirage Royale <tirageroyale55@gmail.com>
2026-05-15 04:51:10 +0200

    correction css card

--- backend/models/CardRequest.js
+++ backend/models/CardRequest.js
@@ -2,18 +2,19 @@ const mongoose = require("mongoose");
 
 const CardRequestSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  cardType: { type: String, required: true }, // ex: "BPER Classic", "BPER Gold"
+  cardType: { type: String, required: true },
   cardNumber: { type: String, required: true },
   expiry: { type: String, required: true },
   cvv: { type: String, required: true },
-  cardBg: { type: String }, // Pour conserver le style visuel choisi
+  cardBg: { type: String },
   logoColor: { type: String },
   status: { 
     type: String, 
-    enum: ["En cours d'investigation", "Validée", "Rejetée"], 
+    // 🔥 AJOUTE "active" ET "blocked" ICI POUR ÉVITER L'ERREUR 500
+    enum: ["En cours d'investigation", "Validée", "Rejetée", "active", "blocked"], 
     default: "En cours d'investigation" 
   },
-  comment: { type: String }, // Note laissée par l'utilisateur
+  comment: { type: String },
   requestDate: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
 });
