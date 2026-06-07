7504f4e1f28bf4fa29610e67ae95d962fe8d7ae2
Tirage Royale <tirageroyale55@gmail.com>
2026-06-07 03:17:46 +0200

    correction pages aide

--- frontend/src/pages/Aide.jsx
+++ frontend/src/pages/Aide.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { api } from "../../services/api";
+import { api } from "../services/api"; 
 import "../../styles/Aide.css"; // Ajustez le chemin vers votre fichier CSS
 
 export default function Aide({ isDesktop = false }) {
