0c97a1fbddb23e2968ba4c4d9cb9f999ab0199a9
Tirage Royale <tirageroyale55@gmail.com>
2026-06-07 17:53:12 +0200

    correction pages aide

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 
 import ProtectedRoute from "./components/ProtectedRoute";
